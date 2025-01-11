3d18b82bd670da728341b08e282d70dbe7439202
Bowen Sun <bowensun@gmail.com>
2025-01-11 00:32:32 +0000

    chore: register downstream cluster in fleet without cluster api

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash -xe
 
-kind create cluster --config kind.yaml
+kind create cluster --config kind-slave.yaml
+kind create cluster --config kind-master.yaml
+gsed -i "s/0.0.0.0/localhost/g" ${HOME}/.kube/config
 
 helm repo add crossplane-stable https://charts.crossplane.io/stable
 helm install crossplane --create-namespace --namespace crossplane-system crossplane-stable/crossplane
@@ -12,33 +14,16 @@ helm install fleet --create-namespace --namespace fleet-system fleet/fleet
 kubectl apply -f fleet/workspaces/local/gitrepo.yaml
 kubectl apply -f fleet/workspaces/downstream/gitrepo.yaml
 
-while [[ ${WAIT_PROVIDER:-false} == "false" || ${WAIT_HELM:-false} == "false" ]]; do
-  sleep 3
-  WAIT_PROVIDER=$(kubectl get providers.pkg.crossplane.io -o yaml | yq '[.items[].status.conditions[].status] | all')
-  WAIT_HELM=$(kubectl get crd -o yaml | yq '[.items[].metadata.name] | contains(["releases.helm.crossplane.io"])')
-done
-while [[ ${WAIT_HELM_RELEASE:-false} == "false" ]]; do
-  sleep 3
-  WAIT_HELM_RELEASE=$(kubectl get releases.helm.crossplane.io -o yaml | yq '[.items[].status.conditions[].status] | all and length > 1')
-done
+#
+# Downstream initiated fleet cluster registration
+#
+while ! kubectl -n fleet-downstream  get secret fleet-downstream; do sleep 5; done
+kubectl --namespace fleet-downstream get secret fleet-downstream -o 'jsonpath={.data.values}' | base64 --decode > .tmp/values.yaml
+yq '.clusters[] | select(.name == "kind-k8s.local.nusnewob.dev").cluster.certificate-authority-data' ~/.kube/config | base64 -d > .tmp/ca.pem
 
-export CLUSTER_TOPOLOGY=true
-export EXP_MACHINE_POOL=true
-export EXP_CLUSTER_RESOURCE_SET=true
-clusterctl init --wait-providers --infrastructure=docker
-
-while [[ ${WAIT_CAPI_WEBHOOK:-false} == "false" ]]; do
-  sleep 3
-  WAIT_CAPI_WEBHOOK=$(kubectl get svc -A -o yaml | yq '[.items[].metadata.name] | contains(["capi-webhook-service","capd-webhook-service","capi-kubeadm-control-plane-webhook-service","capi-kubeadm-bootstrap-webhook-service"])')
-done
-
-NAMESPACE=fleet-downstream
-kubectl apply -n ${NAMESPACE} -f https://raw.githubusercontent.com/kubernetes-sigs/cluster-api/refs/heads/main/test/infrastructure/docker/templates/clusterclass-quick-start.yaml
-
-while [[ ${WAIT_CAPI_CLUSTER_CLASS:-false} == "false" ]]; do
-  sleep 3
-  WAIT_CAPI_CLUSTER_CLASS=$(kubectl get clusterclasses.cluster.x-k8s.io -o yaml | yq '[.items[].status.conditions[].status] | all')
-done
-
-CLUSTER_NAME=slave.local.nusnewob.dev
-clusterctl generate cluster ${CLUSTER_NAME} -n ${NAMESPACE} --kubernetes-version $(kubectl version -o yaml | yq '.serverVersion.gitVersion') --from https://raw.githubusercontent.com/kubernetes-sigs/cluster-api/refs/heads/main/test/infrastructure/docker/templates/cluster-template-development.yaml | kubectl apply -f -
+helm install --kube-context kind-slave --create-namespace -n fleet-system \
+  --values .tmp/values.yaml \
+  --set-string labels.example=true --set-string labels.env=dev \
+  --set-file apiServerCA=.tmp/ca.pem \
+  --set apiServerURL="https://k8s.local.nusnewob.dev-control-plane:6443" \
+  fleet-agent fleet/fleet-agent
